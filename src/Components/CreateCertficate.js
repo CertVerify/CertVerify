2f5ad8e9a4c96708cb7319236ddfbe05cc6a9d43
Genusuppal <harpranav2202@gmail.com>
2023-12-19 22:36:06 +0530

    Generate Certificate Form

--- src/Components/CreateCertficate.js
+++ src/Components/CreateCertficate.js
@@ -1,118 +1,136 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import { ReactComponent as GenIcon } from '../images/icongen.svg'
-import { ReactComponent as DateIcon } from '../images/dategen.svg'
-import { ReactComponent as MarksIcon } from '../images/marksgen.svg'
+import React from "react";
+import Sidebar from "./Sidebar";
+import { ReactComponent as GenIcon } from "../images/icongen.svg";
+import { ReactComponent as DateIcon } from "../images/dategen.svg";
+import { ReactComponent as MarksIcon } from "../images/marksgen.svg";
 // import { ReactComponent as Check } from '../images/test.jpg'
-import { useState } from 'react'
-
-
+import { useState } from "react";
 
 function CreateCertficate() {
-  const [user, Setuser] = useState({
-    name: "", Dob: "", Marks: "",file:null
-  });
+	const [user, setUser] = useState({
+		name: "",
+		dob: "",
+		marks0: "",
+		marks1: "",
+		marks2: "",
+		marks3: "",
+		qr: null,
+		photo: null,
+		sign: null,
+	});
 
-  const handleChange = (e) => {
-    const name = e.target.name;
-    const value = e.target.value;
-  
-    if (name === 'file' && e.target.files.length) {
-      Setuser((prev) => ({
-        ...prev,
-        [name]: e.target.files[0],
-      }));
-    } else {
-      Setuser((prev) => ({
-        ...prev,
-        [name]: value,
-      }));
-    }
-  };
+	const handleChange = (e) => {
+		const name = e.target.name;
+		const value = e.target.value;
 
+		if (name in ["photo", "qr", "sign"] && e.target.files.length) {
+			setUser((prev) => ({
+				...prev,
+				[name]: e.target.files[0],
+			}));
+		} else {
+			setUser((prev) => ({
+				...prev,
+				[name]: value,
+			}));
+		}
+	};
 
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		// console.log(name);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const name = user.name;
-    const marks = user.Marks;
-    const dob = user.Dob;
-    const img = user.file;
-    // console.log(name);
+		const form_send = new FormData();
+		form_send.append("name", user.name);
+		form_send.append("dob", user.dob);
+		form_send.append("marks0", user.marks0);
+		form_send.append("marks1", user.marks1);
+		form_send.append("marks2", user.marks2);
+		form_send.append("marks3", user.marks3);
+		form_send.append("qr", user.qr);
+		form_send.append("photo", user.photo);
+		form_send.append("sign", user.sign);
+		console.log(form_send.getAll("sign"));
+		const response = await fetch("http://localhost:5000/generateCertificate", {
+			method: "POST",
+			body: form_send,
+		});
+		const responseDATA = await response.json();
+		console.log(responseDATA);
+		console.log(user);
+		displayCertificate(responseDATA.certificate);
+	};
 
-    const form_send = {name,dob, marks, img};
-    console.log(form_send);
-    const response = await fetch('http://localhost:5000/generateCertificate',{
-      method: 'POST',
-      body: form_send
-    });
-    const responseDATA = await response.json();
-      console.log(responseDATA);
-    console.log(user);
-    displayCertificate(responseDATA.certificate);
-  };
+	function displayCertificate(certificateBase64) {
+		const certificateContainer = document.getElementById("certificateContainer");
 
-  function displayCertificate(certificateBase64) {
-    const certificateContainer = document.getElementById('certificateContainer');
-    
-    // Create an img element
-    const imgElement = document.createElement('img');
-    
-    // Set the source of the image to the base64 data
-    imgElement.src = 'data:image/png;base64,' + certificateBase64;
+		// Create an img element
+		const imgElement = document.createElement("img");
 
-    // Append the image element to the container
-    certificateContainer.appendChild(imgElement);
+		// Set the source of the image to the base64 data
+		imgElement.src = "data:image/png;base64," + certificateBase64;
 
-}
-  
-  return (
-    <div className='Home_Admin'>
-        <Sidebar />
-        <form className='test' onSubmit={handleSubmit}>
-            <div className='GenText1 textft gentext'>
-                Create a Certificate
-            </div>
-            <div className='PortalText3'>
-                <div className='namename'>
-                  Full Name
-                </div>
-                  <GenIcon />
-                <input type='name' className='Nameinput' name='name'  id='name' onChange={handleChange} placeholder='Enter your full Name'>
-                </input>
-            </div>
-            <div  className='flexit PortalText3 dategen'>
-              <div className='flexset'>
-                <div className='namename'>
-                  Date of Birth
-                </div>
-                  <DateIcon />
-                <input type='date' className='datedate' name='Dob'  id='Dob' onChange={handleChange}placeholder='Enter your full Name'>
-                </input>
-                
-              </div>
-              <div className='flexset2'>
-                <div className='namename marginset'>
-                  Enter Total Marks
-                </div>
-                  <MarksIcon />
-                <input type='number' className='marksmarks' id='marks' name='Marks' onChange={handleChange} placeholder='Marks'>
-                </input>
-              </div>
-            </div>
-            <div>
-              <input type='file' name='file' id='file' onChange={handleChange}>
-              </input>
-            </div>
-            <div className='Buttondiv'>
-              <button type='Submit' className='Create'></button>
-            </div>
-            <div id="certificateContainer"></div>
-        </form>
-    </div>
+		// Append the image element to the container
+		certificateContainer.appendChild(imgElement);
+	}
 
-  )
+	return (
+		<div className="Home_Admin">
+			<Sidebar />
+			<form className="test" onSubmit={handleSubmit}>
+				<div className="GenText1 textft gentext">Create a Certificate</div>
+				<div className="flexset2">
+					<div className="namename">Full Name</div>
+					<GenIcon />
+					<input type="name" className="Nameinput" name="name" onChange={handleChange} placeholder="Enter your full Name" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Date of Birth</div>
+					<DateIcon />
+					<input type="date" className="datedate" name="dob" onChange={handleChange} placeholder="dd/mm/yyyy" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Enter Marks in Subject 1</div>
+					<MarksIcon />
+					<input type="tel" maxLength={3} className="marksmarks" name="marks0" onChange={handleChange} placeholder="Marks out 100" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Enter Marks in Subject 2</div>
+					<MarksIcon />
+					<input type="tel" maxLength={3} className="marksmarks" name="marks1" onChange={handleChange} placeholder="Marks out 100" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Enter Marks in Subject 3</div>
+					<MarksIcon />
+					<input type="tel" maxLength={3} className="marksmarks" name="marks2" onChange={handleChange} placeholder="Marks out 100" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Enter Marks in Subject 4</div>
+					<MarksIcon />
+					<input type="tel" maxLength={3} className="marksmarks" name="marks3" onChange={handleChange} placeholder="Marks out 100" />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Photo</div>
+					<GenIcon />
+					<input type="file" name="photo" onChange={handleChange} />
+				</div>
+				<div className="flexset2">
+					<div className="namename">QR</div>
+					<DateIcon />
+					<input type="file" name="qr" onChange={handleChange} />
+				</div>
+				<div className="flexset2">
+					<div className="namename">Sign</div>
+					<MarksIcon />
+					<input type="file" name="sign" onChange={handleChange} />
+				</div>
+				<div className="Buttondiv">
+					<button type="Submit" className="Create"></button>
+				</div>
+				<div id="certificateContainer"></div>
+			</form>
+		</div>
+	);
 }
 
 export default CreateCertficate;
-
