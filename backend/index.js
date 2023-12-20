55459ff47f01cb33cddcc048dc30c324e6b761e1
Genusuppal <harpranav2202@gmail.com>
2023-12-20 08:37:24 +0530

    Admin Portal Backend Connect

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,77 @@
-// const mongoose = require('mongoose');
-// const DB = 'mongodb+srv://CertVerify:5KKicuWY6yT0tcl9@cluster0.vrxnqdk.mongodb.net/CertVerify?retryWrites=true&w=majority'
+const { MongoClient, ServerApiVersion } = require("mongodb");
+// const uri = "mongodb+srv://njath-admin:2oBazQZRjohsZF4h@njath.pdrlahx.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://CertVerify:5KKicuWY6yT0tcl9@cluster0.vrxnqdk.mongodb.net/?retryWrites=true&w=majority";
 
-// mongoose.connect(DB, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true   
-//   });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+	},
+});
 
-// const UserSchema = new mongoose.Schema({
-//     name: {
-//         type: String,
-//         required: true,
-//     },
-//     email: {
-//         type: String,
-//         required: true,
-//         unique: true,
-//     },
-//     date: {
-//         type: Date,
-//         default: Date.now,
-//     },
-// });
-// const User = mongoose.model('users', UserSchema);
-// User.createIndexes();
- 
+async function readMongo(req, res) {
+	if (req.method === "GET") {
+		try {
+			await client.connect();
+			const institutes = await client.db("CertVerify").collection("institutes").find({ userID: "ritRandom" }).toArray();
+			res.status(200).json(institutes);
+			console.log("Pinged your deployment. You successfully connected to MongoDB!");
+		} catch (error) {
+			res.status(500).json({ error: "Unable to connect to database" });
+		} finally {
+			await client.close();
+		}
+	} else {
+		res.status(405).json({ error: "Unsupported HTTP method" });
+	}
+}
+
+async function updateMongo() {
+	try {
+		await client.connect();
+		const cooll = client.db("CertVerify").collection("institutes");
+		// for await (const doc of cooll.find({ userID: "ritRandom" })) {
+		// 	console.log(doc);
+		// }
+		await cooll.updateOne(
+			{ userID: "ritRandom" },
+			{
+				$push: {
+					certificates: {
+						hashID: "#123456789",
+						name: "Harpranav",
+						certificateID: "CT0987654321",
+						date: "10 Jun, 2022",
+					},
+				},
+			},
+			{}
+		);
+		const cooll2 = client.db("CertVerify").collection("users");
+		await cooll2.updateOne(
+			{ uuid: "123456789012" },
+			{
+				$push: {
+					certificates: {
+						date: "12 Jul, 2027",
+						hashID: "#123456789",
+						issuer: "RIT, Random",
+						name: "BTech - 2027",
+						starred: true,
+						certificateID: "CT0987654321",
+						logo: "https://i.pinimg.com/originals/84/f7/89/84f7895dd0e7e22256a15815322bc5cf.png",
+					},
+				},
+			},
+			{}
+		);
+		console.log("Pinged your deployment. You successfully connected to MongoDB!");
+	} finally {
+		await client.close();
+	}
+}
+// updateMongo().catch(console.dir);
+
 // const express = require('express');
 // const app = express();
 // const cors = require("cors");
@@ -31,10 +79,10 @@
 // app.use(express.json());
 // app.use(cors());
 // app.get("/", (req, resp) => {
- 
+
 //     resp.send("App is Working");
 // });
- 
+
 // app.post("/register", async (req, resp) => {
 //     try {
 //         const user = new User(req.body);
@@ -47,13 +95,11 @@
 //         } else {
 //             console.log("User already register");
 //         }
- 
+
 //     } catch (e) {
 //         resp.send("Something Went Wrong");
 //     }
 // });
-// app.listen(5000);
-
 
 // const express = require('express');
 // const bodyParser = require('body-parser');
@@ -93,19 +139,12 @@
 //     }
 // });
 
-// app.listen(port, () => {
-//     console.log(Server is running on port ${port});
-// });
-
-
-
-
-const express = require('express');
-const axios = require('axios');
-const cors = require('cors');
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
 const Jimp = require("jimp");
-const bodyParser = require('body-parser');
-const multer = require('multer');
+const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const app = express();
 const port = 5000;
@@ -115,180 +154,119 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post('/upload', async (req, res) => {
-    try {
-        const { apiKey, apiSecret, jsonData } = req.body;
+app.get("/getCertsInsti", async (req, res) => {
+	readMongo(req, res);
+})
 
-        if (!apiKey || !apiSecret || !jsonData) {
-            return res.status(400).json({ error: 'Missing required parameters' });
-        }
+app.post("/upload", async (req, res) => {
+	try {
+		const { apiKey, apiSecret, jsonData } = req.body;
 
-        const pinataUrl = 'https://api.pinata.cloud/pinning/pinJSONToIPFS';
+		if (!apiKey || !apiSecret || !jsonData) {
+			return res.status(400).json({ error: "Missing required parameters" });
+		}
 
-        const headers = {
-            'Content-Type': 'application/json',
-            'pinata_api_key': apiKey,
-            'pinata_secret_api_key': apiSecret,
-        };
+		const pinataUrl = "https://api.pinata.cloud/pinning/pinJSONToIPFS";
 
-        const jsonString = JSON.stringify(jsonData);
+		const headers = {
+			"Content-Type": "application/json",
+			pinata_api_key: apiKey,
+			pinata_secret_api_key: apiSecret,
+		};
 
-        const payload = {
-            pinataContent: jsonString,
-        };
+		const jsonString = JSON.stringify(jsonData);
 
-        const response = await axios.post(pinataUrl, payload, { headers });
+		const payload = {
+			pinataContent: jsonString,
+		};
 
-        const pinataUrlPrefix = 'https://gateway.pinata.cloud/ipfs/';
-        const pinataIpfsHash = response.data.IpfsHash;
-        const pinataJsonUrl = pinataUrlPrefix + pinataIpfsHash;
+		const response = await axios.post(pinataUrl, payload, { headers });
 
-        res.json({ pinataUrl: pinataJsonUrl });
-    } catch (error) {
-        console.error('Error:', error.response ? error.response.data : error.message);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+		const pinataUrlPrefix = "https://gateway.pinata.cloud/ipfs/";
+		const pinataIpfsHash = response.data.IpfsHash;
+		const pinataJsonUrl = pinataUrlPrefix + pinataIpfsHash;
+
+		res.json({ pinataUrl: pinataJsonUrl });
+	} catch (error) {
+		console.error("Error:", error.response ? error.response.data : error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
-
-// app.post('/uploadImage', upload.single('image'), async (req, res) => {
-//     try {
-//         const { apiKey, apiSecret } = req.body;
-
-//         if (!apiKey || !apiSecret || !req.file) {
-//             return res.status(400).json({ error: 'Missing required parameters' });
-//         }
-
-//         const pinataUrl = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
-
-//         const headers = {
-//             'Content-Type': 'application/json',
-//             'pinata_api_key': apiKey,
-//             'pinata_secret_api_key': apiSecret,
-//         };
-
-//         const formData = new FormData();
-//         formData.append('file', req.file.buffer, { filename: req.file.originalname });
-
-//         const response = await axios.post(pinataUrl, formData, { headers });
-
-//         const pinataUrlPrefix = 'https://gateway.pinata.cloud/ipfs/';
-//         const pinataIpfsHash = response.data.IpfsHash;
-//         const pinataImageUrl = pinataUrlPrefix + pinataIpfsHash;
-
-//         res.json({ pinataUrl: pinataImageUrl });
-//     } catch (error) {
-
-//         console.error('Error:', error.response ? error.response.data : error.message);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
 
 const storage = multer.memoryStorage();
 const upload1 = multer({ storage: storage });
 
-app.post('/uploadImage', upload1.single('image'), async (req, res) => {
-        try {
-        const { apiKey, apiSecret } = req.body;
+app.post("/uploadImage", upload1.single("image"), async (req, res) => {
+	try {
+		const { apiKey, apiSecret } = req.body;
 
-        if (!apiKey || !apiSecret || !req.file) {
-                return res.status(400).json({ error: 'Missing required parameters' });
-            }
-    
-            const pinataUrl = 'https://api.pinata.cloud/pinning/pinFileToIPFS';
-    
-            const headers = {
-                    'Content-Type': 'multipart/form-data',
-                    'pinata_api_key': apiKey,
-                    'pinata_secret_api_key': apiSecret,
-                };
-        
-                const formData = new FormData();
-                formData.append('file', req.file.buffer, { filename: req.file.originalname });
-        
-                const response = await axios.post(pinataUrl, formData, { headers });
-        
-                const pinataUrlPrefix = 'https://gateway.pinata.cloud/ipfs/';
-                const pinataIpfsHash = response.data.IpfsHash;
-                const pinataImageUrl = pinataUrlPrefix + pinataIpfsHash;
-        
-                res.json({ pinataUrl: pinataImageUrl });
-            } catch (error) {
-                    console.error('Error:', error.response ? error.response.data : error.message);
-                    res.status(500).json({ error: 'Internal Server Error' });
-                }
-            });
-            
-            
-            
-            ///////////////////////////////////////////
-            
-            
-            const upload = multer({ storage: storage }).array('img', 2);
-            
-            app.post("/generateCertificate",upload, async (req, res) => {
-                try {
-        const {name , dob, phy_marks, maths_marks, chem_marks, socio_marks, date} = req.body;
+		if (!apiKey || !apiSecret || !req.file) {
+			return res.status(400).json({ error: "Missing required parameters" });
+		}
 
-        const image = await Jimp.read("./Marksheet.png");
-        const sign = await Jimp.read(req.files[0].buffer);
-        const photo = await Jimp.read(req.files[1].buffer);
+		const pinataUrl = "https://api.pinata.cloud/pinning/pinFileToIPFS";
 
-        const font1 = await Jimp.loadFont(Jimp.FONT_SANS_8_BLACK);
-        const font2 = await Jimp.loadFont(Jimp.FONT_SANS_12_BLACK);
-        image.print(font1,105,164, name);
-        image.print(font1,133,179, dob);
-        image.print(font1,110,439, date);
-        image.print(font2,233,271, maths_marks);
-        image.print(font2,233,302, socio_marks);
-        image.print(font2,233,332, chem_marks);
-        image.print(font2,233,363, phy_marks);
+		const headers = {
+			"Content-Type": "multipart/form-data",
+			pinata_api_key: apiKey,
+			pinata_secret_api_key: apiSecret,
+		};
 
-        // sign.resize(300, 150);
-        // image.blit(sign, 1280, 980);
+		const formData = new FormData();
+		formData.append("file", req.file.buffer, { filename: req.file.originalname });
 
-        sign.resize(80,50);
-        photo.resize(80,80);
-        image.blit(photo, 428, 80);
-        image.blit(sign, 413, 520);
+		const response = await axios.post(pinataUrl, formData, { headers });
 
-        const certificateBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
-        // res.type('png').send(certificateBuffer);
-        res.json({ success: true, certificate: certificateBuffer.toString('base64') });
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Internal Server Error");
-    }
+		const pinataUrlPrefix = "https://gateway.pinata.cloud/ipfs/";
+		const pinataIpfsHash = response.data.IpfsHash;
+		const pinataImageUrl = pinataUrlPrefix + pinataIpfsHash;
+
+		res.json({ pinataUrl: pinataImageUrl });
+	} catch (error) {
+		console.error("Error:", error.response ? error.response.data : error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+	}
 });
 
+///////////////////////////////////////////
 
+const upload = multer({ storage: storage }).array("img", 2);
+
+app.post("/generateCertificate", upload, async (req, res) => {
+	try {
+		const { name, dob, phy_marks, maths_marks, chem_marks, socio_marks, date } = req.body;
+
+		const image = await Jimp.read("./Marksheet.png");
+		const sign = await Jimp.read(req.files[0].buffer);
+		const photo = await Jimp.read(req.files[1].buffer);
+
+		const font1 = await Jimp.loadFont(Jimp.FONT_SANS_8_BLACK);
+		const font2 = await Jimp.loadFont(Jimp.FONT_SANS_12_BLACK);
+		image.print(font1, 105, 164, name);
+		image.print(font1, 133, 179, dob);
+		image.print(font1, 110, 439, date);
+		image.print(font2, 233, 271, maths_marks);
+		image.print(font2, 233, 302, socio_marks);
+		image.print(font2, 233, 332, chem_marks);
+		image.print(font2, 233, 363, phy_marks);
+
+		// sign.resize(300, 150);
+		// image.blit(sign, 1280, 980);
+
+		sign.resize(80, 50);
+		photo.resize(80, 80);
+		image.blit(photo, 428, 80);
+		image.blit(sign, 413, 520);
+
+		const certificateBuffer = await image.getBufferAsync(Jimp.MIME_PNG);
+		// res.type('png').send(certificateBuffer);
+		res.json({ success: true, certificate: certificateBuffer.toString("base64") });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Internal Server Error");
+	}
+});
 
 app.listen(port, () => {
-    console.log('Server is running at http://localhost:${port}');
+	console.log("Server is running at http://localhost:${port}");
 });
--- src/Components/GeneratedCertificate.js
+++ src/Components/GeneratedCertificate.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as Name } from '../images/Name.svg';
 import { ReactComponent as Certificate_Id} from '../images/Certificate_Id.svg';
 import { ReactComponent as Issue_On} from '../images/Issued_On.svg';
@@ -6,10 +6,26 @@ import { ReactComponent as Line} from '../images/Line.svg';
 
 const GeneratedCertificate = () => {
   const [data, setData] = useState([
-    {name: 'Raju Bhaiya', certificate_id: 'CF78468', date: '12 Dec 2004' },
-    {name: 'Krishna', certificate_id: 'CF78568', date: '12 Dec 2004' },
-    {name: 'Satyam Raj', certificate_id: 'CF78668', date: '12 Dec 2004' },
   ]);
+  
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+	  if (isLoading)
+		  fetch('http://localhost:5000/getCertsInsti', {
+			  method: 'GET',
+		  })
+			  .then((data) => data.json())
+			  .then((dataJSON) =>
+				  dataJSON.length == 0
+					  ? []
+					  : dataJSON[0].certificates
+			  )
+			  .then((processedData) => {
+				  setData(processedData);
+				  setLoading(false);
+			  });
+  });
 
   return (
     <table className='Table'>
@@ -23,9 +39,9 @@ const GeneratedCertificate = () => {
       {/* <Line /> */}
       <tbody>
         {data.map((row) => (
-          <tr key={row.certificate_id}>
+          <tr key={row.certificateID}>
             <td className='TableName'>{row.name}</td>
-            <td className='TableCertId'>{row.certificate_id}</td>
+            <td className='TableCertId'>{row.certificateID}</td>
             <td>{row.date}</td>
           </tr>
         ))}
